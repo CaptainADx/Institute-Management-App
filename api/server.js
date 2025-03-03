5cfa71be3085889b21941dc919de426163156151
CaptainADx <captainabhijeet567@gmail.com>
2025-03-03 09:50:15 +0530

    use async await and replaced the Promise(then and catch statements)

--- api/server.js
+++ api/server.js
@@ -6,3 +6,11 @@ const server = http.createServer(app);
 server.listen(port, ()=>{
     console.log('This app is running . . .');
 });
+
+
+
+// const express=require("express");
+
+// const app=express;
+
+// app.listen(8000,()=>console.log("server started at 8000"));
